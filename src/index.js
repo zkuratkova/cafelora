a35fe87c4b1db40fca85f2d818efb7b849fd72e0
Zuzana Kuřátková <zkuratkova@gmail.com>
2021-04-24 12:52:52 +0200

    Tlačítko Objednat, Zrušit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ console.log('funguju!');
 
 const navBtnElm = document.querySelector('#nav-btn');
 const navElm = document.querySelector('nav');
+const orderBtnElm = document.querySelector('.order-btn');
+const drinkCupElm = document.querySelector('.drink__cup');
 
 navBtnElm.addEventListener('click', () => {
   navElm.classList.toggle('nav-closed');
@@ -15,4 +17,18 @@ aElms.forEach((tlacitko) => {
   tlacitko.addEventListener('click', () => {
     navElm.classList.toggle('nav-closed');
   });
+});
+
+let ordered = false;
+
+orderBtnElm.addEventListener('click', () => {
+  if (!ordered) {
+    drinkCupElm.classList.add('drink__cup--selected');
+    orderBtnElm.textContent = 'Zrušit';
+    ordered = true;
+  } else {
+    drinkCupElm.classList.remove('drink__cup--selected');
+    orderBtnElm.textContent = 'Objednat';
+    ordered = false;
+  }
 });
