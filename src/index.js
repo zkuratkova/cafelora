402187f3ae27c0b3b1cd1ddb51463a5dc5050fec
Zuzana Kuřátková <zkuratkova@gmail.com>
2021-04-24 12:07:14 +0200

    zprovoznění skrývání a zobrazování navigace

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import './style.css';
 
 console.log('funguju!');
+
+const navBtnElm = document.querySelector('#nav-btn');
+const navElm = document.querySelector('nav');
+
+navBtnElm.addEventListener('click', () => {
+  navElm.classList.toggle('nav-closed');
+});
+
+const aElms = document.querySelectorAll('a');
+
+aElms.forEach((tlacitko) => {
+  tlacitko.addEventListener('click', () => {
+    navElm.classList.toggle('nav-closed');
+  });
+});
